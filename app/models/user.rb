97005f58abf30319aacfba03e7ef44957570f643
sbigelo <sean.bigelow1@gmail.com>
2021-02-23 14:57:25 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments << comment
+    end
+  end
+
 end
